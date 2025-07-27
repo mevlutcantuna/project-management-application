2a6c046419f17a5c1aa136c77b1da5db35c53290
Mevlüt Can Tuna <mttuna90@gmail.com>
2025-07-27 13:34:09 +0300

    feat(client): add testing guidelines for Vitest, React Testing Library, and MSW

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { Button } from "./components/ui/button";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <>
-      <Button className="m-5">Click me</Button>
-    </>
+    <div className="m-5 flex items-center gap-4">
+      <Button onClick={() => setCount(0)}>Reset</Button>
+      <Button
+        onClick={() => {
+          setCount((prev) => prev - 1);
+        }}
+      >
+        Decrement
+      </Button>
+
+      <p>Count: {count}</p>
+      <Button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Increment
+      </Button>
+    </div>
   );
 }
 
