b99de0d4822943aaee6caa4ee2eca5eccdadb7e3
Mevlüt Can Tuna <mttuna90@gmail.com>
2025-08-20 16:36:23 +0300

    feat(client): improve error handling in authentication forms and enhance user feedback

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/shared/routes/route";
 import { Toaster } from "./components/ui/sonner";
+import { toast } from "sonner";
+import { getErrorMessage } from "./shared/lib/utils";
+import { AxiosError } from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,13 +21,27 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
+  mutationCache: new MutationCache({
+    onError: (error: unknown) => {
+      // Skip error toast for auth endpoints (login/signup)
+      if (
+        error instanceof AxiosError &&
+        !error?.request?.responseURL.includes("/login") &&
+        !error?.request?.responseURL.includes("/signup")
+      ) {
+        toast.error("Error", {
+          description: getErrorMessage(error),
+        });
+      }
+    },
+  }),
 });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <Toaster position="bottom-right" />
+      <Toaster position="top-right" />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
